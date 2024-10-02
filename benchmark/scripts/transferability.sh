83906aee2e6ef376fd0569447cccd33835f0d110
Rock <59243884+rockcor@users.noreply.github.com>
2024-10-02 02:39:38 -0400

    Update transferability.sh

--- benchmark/scripts/transferability.sh
+++ benchmark/scripts/transferability.sh
@@ -1,6 +1,6 @@
 # Transferability
 # Must obtain condensed graph of each methods by running performance.sh before running this script.
-for method in random kcenter averaging vng gcondx geom sfgc doscond gcond msgc sgdd; do
+for method in random kcenter averaging vng gcondx geom sfgc doscond gcond msgc sgdd gcdm; do
   for dataset in cora ogbn-arxiv reddit; do
       python ../run_cross_arch.py -M $method -D $dataset
   done
